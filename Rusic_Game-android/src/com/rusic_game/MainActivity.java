efad0d7daf2f68a0a9ccd8e4eae655cdbb61d148
Anibal Castineyra <anibalhack@gmail.com>
2013-10-26 15:16:41 -0400

    Got the problem fixed for the powers of 2, I hope

--- Rusic_Game-android/src/com/rusic_game/MainActivity.java
+++ Rusic_Game-android/src/com/rusic_game/MainActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 
 public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        Texture.setEnforcePotImages(false);
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useAccelerometer = false;
         cfg.useCompass = false;
