33cdf28c3c127110e68ea3845bb7e478765a9277
Wilson Yeung <wyeung@ufl.edu>
2013-10-29 16:38:44 -0400

    Added delay on visualizer rendering

--- Rusic_Game/src/com/rusic_game/models/Visualizer.java
+++ Rusic_Game/src/com/rusic_game/models/Visualizer.java
@@ -20,7 +20,8 @@ public class Visualizer {
 	private Fixture[] groundBarFixtures;
 	private Fixture[] ceilingBarFixtures;
 	private FixtureDef fixtureDef;
-	
+	private int delay;
+
 	public Visualizer(World world, AudioAnalyzer analyzer) {
 		this.world = world;
 		this.analyzer = analyzer;
@@ -30,75 +31,86 @@ public class Visualizer {
 		groundBarFixtures = new Fixture[numBars];
 		ceilingBarFixtures = new Fixture[numBars];
 		fixtureDef = new FixtureDef();
-        fixtureDef.restitution = 0f;
-        fixtureDef.friction = .5f;
-        fixtureDef.density = 1000000;
-        
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox((width / numBars) / 2, 0);
+		fixtureDef.restitution = 0f;
+		fixtureDef.friction = .5f;
+		fixtureDef.density = 1000000;
+		delay = 0;
 
-        fixtureDef.shape = shape;
-		
-		for(int i = 0; i < numBars; i++) {
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox((width / numBars) / 2, 0);
+
+		fixtureDef.shape = shape;
+
+		for (int i = 0; i < numBars; i++) {
 			BodyDef groundBarDef = new BodyDef();
 			BodyDef ceilingBarDef = new BodyDef();
 			groundBarDef.type = BodyType.StaticBody;
 			ceilingBarDef.type = BodyType.StaticBody;
-			if(i < numBars / 2) {
-				groundBarDef.position.set(-(width * (numBars - i)/numBars - width/2), -height/2);
-				ceilingBarDef.position.set(-(width * (numBars - i)/numBars - width/2), height/2);
+			if (i < numBars / 2) {
+				groundBarDef.position.set(
+						-(width * (numBars - i) / numBars - width / 2),
+						-height / 2);
+				ceilingBarDef.position.set(
+						-(width * (numBars - i) / numBars - width / 2),
+						height / 2);
+			} else {
+				groundBarDef.position.set(width * i / numBars - width / 2,
+						-height / 2);
+				ceilingBarDef.position.set(width * i / numBars - width / 2,
+						height / 2);
 			}
-			else {
-				groundBarDef.position.set(width * i/numBars - width/2, -height/2);
-				ceilingBarDef.position.set(width * i/numBars - width/2, height/2);
-			}
-            groundBarDef.fixedRotation = true;
-            ceilingBarDef.fixedRotation = true;
+			groundBarDef.fixedRotation = true;
+			ceilingBarDef.fixedRotation = true;
 			groundBars[i] = world.createBody(groundBarDef);
 			ceilingBars[i] = world.createBody(ceilingBarDef);
-            
+
 			groundBarFixtures[i] = groundBars[i].createFixture(fixtureDef);
 			ceilingBarFixtures[i] = ceilingBars[i].createFixture(fixtureDef);
 		}
 		shape.dispose();
 	}
-	
-	public void update() {
-		
-		analyzer.getSpectrum(spectrum);
-		for(int i = 0; i < numBars; i++) {
-			int histoX = 0;
-			if(i < numBars / 2) {
-				histoX = numBars/2 - i;
-			}
-			else {
-				histoX = i - numBars/2;
-			}
-			int nb = (spectrum.length / numBars) / 2;
-			groundBars[i].destroyFixture(groundBarFixtures[i]);
-			ceilingBars[i].destroyFixture(ceilingBarFixtures[i]);
-			
-			PolygonShape shape = new PolygonShape();
-            shape.setAsBox((width / numBars) / 2, scale(avg(histoX, nb)) / 2);
 
-            fixtureDef.shape = shape;
-            
-			groundBarFixtures[i] = groundBars[i].createFixture(fixtureDef);
-			ceilingBarFixtures[i] = ceilingBars[i].createFixture(fixtureDef);
-			shape.dispose();
+	public void update() {
+		if (delay > 6) {
+			analyzer.getSpectrum(spectrum);
+			for (int i = 0; i < numBars; i++) {
+				int histoX = 0;
+				if (i < numBars / 2) {
+					histoX = numBars / 2 - i;
+				} else {
+					histoX = i - numBars / 2;
+				}
+				int nb = (spectrum.length / numBars) / 2;
+				groundBars[i].destroyFixture(groundBarFixtures[i]);
+				ceilingBars[i].destroyFixture(ceilingBarFixtures[i]);
+
+				PolygonShape shape = new PolygonShape();
+				shape.setAsBox((width / numBars) / 2,
+						scale(avg(histoX, nb)) / 2);
+
+				fixtureDef.shape = shape;
+
+				groundBarFixtures[i] = groundBars[i].createFixture(fixtureDef);
+				ceilingBarFixtures[i] = ceilingBars[i]
+						.createFixture(fixtureDef);
+				shape.dispose();
+			}
+			delay = 0;
+		}else{
+			delay++;
 		}
 	}
-	
+
 	private float scale(float x) {
-        return x / 256 * height * 2.0f;
+		return x / 256 * height * 2.0f;
 	}
 
 	private float avg(int pos, int nb) {
-        int sum = 0;
-        for (int i = 0; i < nb; i++) {
-                sum += spectrum[pos + i];
-        }
+		int sum = 0;
+		for (int i = 0; i < nb; i++) {
+			sum += spectrum[pos + i];
+		}
 
-        return (float) (sum / nb);
+		return (float) (sum / nb);
 	}
 }
